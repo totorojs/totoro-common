2fb7b51144c2e141cbca81aa458f1c9391e8d456
kangpangpang <kangpangpang@gmail.com>
2013-07-17 18:33:33 +0800

    A: add tatal method

--- logger.js
+++ logger.js
@@ -15,7 +15,7 @@ var debug = process.argv.some(function(arg) {
     return arg === '--verbose'
 })
 
-var levels = ['LOG', 'TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR']
+var levels = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL']
 var level = 'INFO'
 
 if (debug) {
@@ -70,24 +70,18 @@ function _push2File(str) {
     logFile.write(str)
 }
 
-var errorFormat
-if (debug) {
-    errorFormat = '{{message}} (in {{file}}:{{line}})\nCall Stack:{{stacklist}}'
-} else {
-    errorFormat = '{{message}} (in {{file}}:{{line}})'
-}
-
 
 var logger = tracer.colorConsole({
-    level: 'log',
+    methods : ['debug', 'info', 'warn', 'error', 'fatal'],
+    level: 'debug',
     format: [
         '{{title}} {{file}}:{{line}} | {{message}}', //default format
         {
-            error : errorFormat
+            fatal: '{{message}} (in {{file}}:{{line}})\nCall Stack:{{stacklist}}'
         }
     ],
     preprocess: function(data) {
-        if (data.title === 'error') {
+        if (data.title === 'fatal') {
             var callstack = '', len = data.stack.length
             for (var i = 0; i < len; i++) {
                 callstack += '\n' + data.stack[i]
@@ -97,7 +91,6 @@ var logger = tracer.colorConsole({
         }
     },
     filters: [{
-        trace : colors.magenta,
         info : colors.green,
         warn : colors.yellow,
         error : [colors.red]
@@ -112,7 +105,7 @@ var logger = tracer.colorConsole({
             _push2File(getMsg(title, data))
         }
 
-        if (title === 'ERROR') {
+        if (title === 'FATAL') {
             process.exit(0)
         }
     }
@@ -121,10 +114,10 @@ var logger = tracer.colorConsole({
 function getMsg(title, data) {
     var msg = getPrefix(title, data) +  ' | '
 
-    if (title !== 'ERROR') {
+    if (title !== 'FATAL') {
         return msg + data.message
     } else {
-        return msg + '[' + data.stack.split('\n').join(',') + ']'
+        return msg + '[' + data.stacklist.split('\n').join(',') + ']'
     }
 }
 
